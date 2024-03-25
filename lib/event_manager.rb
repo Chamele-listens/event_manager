c8eeaa03154381fd5e85caa2f166dd08dae28bec
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-03-25 17:04:31 +0700

    Add clean_zipcode methods

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,21 @@
-puts 'Event Manager Initialized!'
+require 'csv'
 
-lines = File.readlines('event_attendees.csv')
-lines.each do |line|
-    next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-    columns = line.split(",")
-    name = columns[2]
-    puts name
+def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
 end
